5b70df025931637e373a1d508dcf57488659f225
kyohei.shibuya <kyohei.shibuya@diddyworks.co.jp>
2021-03-08 03:10:41 +0900

    インポート用ファイル追加

---
+++ lib/import_race_data.rb
@@ -0,0 +1,90 @@
+require 'bundler/setup'
+require 'nokogiri'
+require 'open-uri'
+require 'csv'
+
+class ImportRaceData
+
+  INDEX_YEAR = [2020, 2021] # データを取得したい年
+  CSV_HEADER = ['日時', 'レース名', 'グレード', '着順', '馬名', '年齢', '騎手', '調教師']
+
+  def setup_doc(url)
+    doc = Nokogiri::HTML.parse(URI.open(url, "r:CP932").read)
+    doc.search('br').each { |n| n.replace("\n") }
+    doc
+  end
+
+  def scrape_data(data)
+    horse_data = []
+    # 着順
+    horse_data << data.children[1].text
+    # 馬名
+    horse_data << data.children[7].text
+    # 年齢
+    horse_data << data.children[9].text
+    # 騎手
+    horse_data << data.children[13].text
+    # 調教師
+    horse_data << data.children[25].text
+
+    horse_data.map(&:strip)
+  end
+
+  def race_info(doc)
+    race_info = []
+    # 日時
+    race_info << doc.xpath("//div[@class='cell date']").text.split('（').first
+    # レース名
+    race_info << doc.xpath("//span[@class='race_name']").text
+    # グレード
+    race_info << doc.xpath("//span[@class='grade_icon lg']").first.children.attribute('alt').value
+
+    race_info.map(&:strip)
+  end
+
+    # 結果ページURLの配列を生成
+  def create_result_urls(doc)
+    table_data = doc.xpath("id('contentsBody')/div[@class='scr-md']/table[@class='basic narrow-xy striped mt20']/tbody[@class='td_left']/tr")
+    urls = table_data.map do |data|
+      result_path = data&.children[15]&.children[0]&.values&.first
+      "https://www.jra.go.jp/#{result_path}" if result_path.present?
+    end
+
+    urls.compact
+  end
+
+  # 年から重賞ページの配列を生成
+  def index_urls(years = [])
+    urls = years&.map do |year|
+      "https://www.jra.go.jp/datafile/seiseki/replay/#{year}/jyusyo.html"
+    end
+
+    urls
+  end
+
+  def exec
+    index_url_list = index_urls(INDEX_YEAR)
+    urls = index_url_list.map do |index_url|
+      index_doc = setup_doc(index_url)
+      create_result_urls(index_doc)
+    end.flatten
+
+    csv_header = ['日時', 'レース名', 'グレード', '着順', '馬名', '年齢', '騎手', '調教師']
+
+    CSV.open('./lib/result.csv', 'w') do |csv|
+      csv << CSV_HEADER
+      urls.each do |url|
+        begin
+          doc = setup_doc(url)
+          race_info_columns = race_info(doc)
+          table_data = doc.xpath("id('race_result')/div[@class='race_result_unit']/table[@class='basic narrow-xy striped']/tbody[1]/tr")
+          table_data.each do |data|
+            csv << race_info_columns + scrape_data(data)
+          end
+        rescue => e
+          p e
+        end
+      end
+    end
+  end
+end
